bd3080398a312218392c973e50152567e3a56639
He (Damon) Chen <pterofighter1139283368@gmail.com>
2023-04-11 22:29:40 -0700

    got models working and inserting into local database

--- apptracker/settings.py
+++ apptracker/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apptracker',
+    'trackerapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,12 +75,20 @@ WSGI_APPLICATION = 'apptracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'apptracker',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
