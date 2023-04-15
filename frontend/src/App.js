f8481e8c5ed48589275627497cb6f0aae28eff3c
chan34kelvin <chankelvin74@gmail.comhan34kelvin[B>
2023-04-15 01:18:16 -0700

    did container-presentational layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Views/DashboardView.js';
+import DashboardContainer from "./containers/DashboardContainer/DashboardContainer.js";
 import './App.css'
 
 export default function App() {
+
+  //reason why this is wrapped with fragment (ex: <> </>)
+  //because it won't be affected by any css, any it can put <navbar> on top of <router>
   return (
-    <Router>
+    <>
+      {/* <Navbar /> */}
+      <Router>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/" element={<DashboardContainer/>} />
         </Routes>
-    </Router>
+      </Router>
+    </>
   );
 }
 
--- frontend/src/Views/DashboardView.js
+++ frontend/src/Views/DashboardView.js
@@ -1,10 +1,12 @@
+import React from "react";
 
-
-export default function DashboardView() {
+export default function DashboardView( {applications} ) {
+    
+    
     return (
-        <div>
+        <>
             
-        </div>
+        </>
         //make the offcanvas navbar first
         //then start making the layout for the dashboard 
         //there are placeholders for loading screens
