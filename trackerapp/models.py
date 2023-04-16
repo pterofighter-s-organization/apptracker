f44fc461a38520826a294ef3c3723dfb7df6d0c0
He (Damon) Chen <pterofighter1139283368@gmail.com>
2023-04-15 22:16:43 -0700

    started working more on the models

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -5,3 +5,18 @@ class Users(models.Model):
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+
+class Application(models.Model):
+    application_id = models.ObjectIdField()
+    user_id = models.ObjectIdField()
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=2048)
+    status = models.IntegerField()
+    date = models.DateField()
+    salary = models.IntegerField()
+    
+
+class notes(models.Model):
+    note_id = models.ObjectIdField()
+    application_id = models.ObjectIdField()
+    note = models.CharField(max_length=2048)
