acfc5d04463773003808499536ed95f6e719c857
chan34kelvin <chankelvin74@gmail.comhan34kelvin[B>
2023-04-16 17:03:39 -0700

    changed the folder structures to individual component folders with its own container and presentation

---
+++ frontend/src/components/Dashboard/DashboardContainer.js
@@ -0,0 +1,15 @@
+import UrgentTaskContainer from "../UrgentTask/UrgentTaskContainer.js"
+import ApplicationCardContainer from "../ApplicationCard/ApplicationCardContainer.js"
+
+export default function DashboardContainer( props ) {
+
+    //we going to define a simple layout grid here for these two containers
+
+    return(
+        <>
+            <h1>Dashboard</h1>
+            <UrgentTaskContainer/>
+            <ApplicationCardContainer/>
+        </>
+    )
+}
