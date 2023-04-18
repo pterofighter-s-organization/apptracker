a0086bc4b5b7abb578c5eedb7e2abc0ced1074ba
chan34kelvin <chankelvin74@gmail.comhan34kelvin[B>
2023-04-18 04:09:34 -0700

    read urgentaskcontainer and follow that to code it

--- frontend/src/components/UrgentTask/UrgentTaskContainer.js
+++ frontend/src/components/UrgentTask/UrgentTaskContainer.js
@@ -8,6 +8,7 @@ export default function UrgentTaskContainer({}){
     //make a container that only fetches data
     //so this container pass on the status to take to the list container
     //this container only categorizes the apps //categorizecontainer
+    //so the list presentation decides how the list looks not the container
     return(
         <>
             <CategorizeCardContainer/>
