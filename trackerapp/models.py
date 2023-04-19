5b9d9830a128ea9e8b926839c944ad7757e48935
chan34kelvin <chankelvin74@gmail.comhan34kelvin[B>
2023-04-19 02:40:18 -0700

    updated backend 4-19 (pull a new one at 4/21)

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -5,3 +5,4 @@ class Users(models.Model):
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    #add more to this later by pulling
