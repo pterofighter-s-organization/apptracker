a1da085fe01c72c51e946ea7156a1b68ac41996a
He (Damon) Chen <pterofighter1139283368@gmail.com>
2023-04-18 23:37:56 -0700

    updated the models again

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -2,20 +2,23 @@ from djongo import models
 
 # Create your models here.
 class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField()
     password = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Application(models.Model):
-    application_id = models.ObjectIdField()
-    user_id = models.ObjectIdField()
+    application_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=2048)
     status = models.CharField(max_length=255)
     date = models.DateField()
     salary = models.IntegerField()
     
-class notes(models.Model):
-    note_id = models.ObjectIdField()
-    application_id = models.ObjectIdField()
+class Notes(models.Model):
+    note_id = models.AutoField(primary_key=True)
+    application_id = models.IntegerField()
     note = models.CharField(max_length=2048)
--- trackerapp/serializers.py
+++ trackerapp/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from models import Users
 
-class TodoSerializer(serializers.ModelSerializer):
+class Serializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ('id', 'username', 'email', 'password')
+
