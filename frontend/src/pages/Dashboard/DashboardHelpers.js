14237b55e9d4c17781f02abadd3ea34ca84fdaae
chan34kelvin <chankelvin74@gmail.comhan34kelvin[B>
2023-05-02 22:52:05 -0700

    made buttons and bg collapse component

--- frontend/src/pages/Dashboard/DashboardHelpers.js
+++ frontend/src/pages/Dashboard/DashboardHelpers.js
@@ -1,8 +1,7 @@
 
 //this is a file for helper functions (PURE FUNCTIONS (no side effect, no global vars used) inside dashboard.js)
-//only used for dashboard.js
+//only used for dashboard.js and designed for it
 
-//later these can be its own seperate component * (later) gotta think about it
 export function checkShowCollapseApps (apps, windowWidth) {
 
     //calculate how many cards can the current window can fit to see if theres a need for collapse
@@ -15,16 +14,17 @@ export function checkShowCollapseApps (apps, windowWidth) {
     //gap is gutter width = container width * gutter percentage / 100
     //the percentage is 1.5 usually but i downscale to 1.25 for margins (IMPORTANT DONT DELETE)
     const gap = (containerWidth * 1.25 / 100)
-    //get the card width from appcard.css
-    const cardsToFit = Math.floor(windowWidth / (315 + gap))
+
+    const cardsToFit = Math.floor(windowWidth / (315 + gap)) //get the card width from appcard.css
 
     //315px card
     //so if the with of the window can fit all the cards then no need the button
-
     return !(cardsToFit >= apps.length)
 }
 
 export function checkShowCollapseTasks (tasks) {
+
     //if the amount of tasks is > 3 that means there gonna be alot of task
+    
     return (tasks.length > 5)
 }
