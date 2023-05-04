a5b9f80c97242b628cb25b7f9614a994b3f68bf2
He (Damon) Chen <pterofighter1139283368@gmail.com>
2023-05-03 20:49:09 -0700

    updated models and migration with users and notes

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    # user_id = models.ObjectIdField()
+    user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
@@ -11,20 +11,19 @@ class Users(models.Model):
         return self.name
     
 class Notes(models.Model):
+    note_id = models.AutoField(primary_key=True)
+    application_id = models.IntegerField()
     title = models.CharField(max_length=255)
     note = models.CharField(max_length=2048)
-    class Meta:
-        abstract = True
+
+    
 
 class Application(models.Model):
     application_id = models.AutoField(primary_key=True)
-    # user_id = models.ObjectIdField()
+    user_id = models.IntegerField()
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=2048)
     status = models.CharField(max_length=255)
     date = models.DateField()
     salary = models.IntegerField()
-    # note = models.EmbeddedField(
-    #     model_container = Notes,
-    # )
     
