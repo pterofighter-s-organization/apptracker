75b207666344bdf6822c5e0a34b43691a26df49d
chan34kelvin <chankelvin74@gmail.comhan34kelvin[B>
2023-05-11 06:41:42 -0700

    fixed collapse

--- frontend/src/components/PreviewCollapse/PreviewCollapse.js
+++ frontend/src/components/PreviewCollapse/PreviewCollapse.js
@@ -64,7 +64,7 @@ export default function PreviewCollapse(props) {
                 const show = vhToPx < containerRef.clientHeight
 
                 //debugging, the container in px, parentcontainer in px, and parentcontainer in px int
-                console.log(containerRef.clientHeight, getComputedStyle(collapseRef).maxHeight, parseInt(collapseRef.offsetHeight))
+                // console.log(vhToPx, containerRef.clientHeight, getComputedStyle(collapseRef).maxHeight, parseInt(collapseRef.offsetHeight), collapseId)
 
                 setShowCollapse(show)
             }
