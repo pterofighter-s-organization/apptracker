61b7260e9b69def3593a7057093d0d39be41e1ac
chan34kelvin <chankelvin74@gmail.comhan34kelvin[B>
2023-06-06 08:10:10 -0700

    fixed imports

--- frontend/archived/hooks/useWindowSizeManager.js
+++ frontend/archived/hooks/useWindowSizeManager.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 
 //utils
-import { debounce } from "../utils/dateTime/time/time"
 
 export default function useWindowSizeManager() {
 
@@ -10,23 +9,23 @@ export default function useWindowSizeManager() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
     const [windowHeight, setWindowHeight] = useState(window.innerHeight)
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        //delay 250 secs after the user starts resizing to start using the function
-        const windowSizeChange = debounce(() => {
-            console.log("resize")
-            setWindowWidth(window.innerWidth)
-            setWindowHeight(window.innerHeight)
-        }, 250)
+    //     //delay 250 secs after the user starts resizing to start using the function
+    //     // const windowSizeChange = debounce(() => {
+    //     //     console.log("resize")
+    //     //     setWindowWidth(window.innerWidth)
+    //     //     setWindowHeight(window.innerHeight)
+    //     // }, 250)
 
-        window.addEventListener("resize", windowSizeChange)
-        //demount, making sure no memory leaks
-        //this is why I keep getting errors because there were multiple instances running
-        //causing unexpected behaviours
-        return (
-            () => window.removeEventListener("resize", windowSizeChange)
-        )
-    }, [])
+    //     window.addEventListener("resize", windowSizeChange)
+    //     //demount, making sure no memory leaks
+    //     //this is why I keep getting errors because there were multiple instances running
+    //     //causing unexpected behaviours
+    //     return (
+    //         () => window.removeEventListener("resize", windowSizeChange)
+    //     )
+    // }, [])
 
     return {
         windowWidth,
--- frontend/archived/index.js
+++ frontend/archived/index.js
@@ -11,14 +11,14 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 // import "bootstrap/js/dist/offcanvas.js";
 // import "bootstrap/js/dist/collapse.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
