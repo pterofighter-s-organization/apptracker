9fa9c53c2e7ecfe6cd612ae11ae0b823da4bd732
Kelvin Chan <31221043+chan34kelvin@users.noreply.github.com>
2023-11-08 00:19:20 +0800

    modified task card width change at md

---
+++ frontend/src/pages/NoteBoard/NoteBoard.js
@@ -0,0 +1,19 @@
+
+
+
+//layouts
+import { HeaderLayout } from "../../layouts/HeaderLayout";
+import { PageLayout } from "../../layouts/PageLayout";
+
+
+
+export default function NoteBoard(){
+
+    return(
+        <PageLayout>
+            <HeaderLayout title={"all job notes"}>
+
+            </HeaderLayout>
+        </PageLayout>
+    )
+}
