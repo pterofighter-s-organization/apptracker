d9451c7b5b0f8078626fc2c0f51296baf867505c
Kelvin Chan <31221043+chan34kelvin@users.noreply.github.com>
2023-12-09 23:29:12 +0800

    fixed css not working on firefox correctly

--- frontend/src/components/NotificationList/NotificationList.js
+++ frontend/src/components/NotificationList/NotificationList.js
@@ -4,13 +4,14 @@
 //css
 import "./NotificationList.css"
 
-export default function NotificationList(){
+export default function NotificationList() {
 
     //this will be where the notifications are stored
-    return(
-        <div 
-            className="notification-list" 
+    return (
+        <div
+            className="notification-list"
             id={"notification-list"}
-        />
+        >
+        </div>
     )
 }
